1220d4fd1f16b074e08d7934be07a1147cf011ca
Samuel Bustos <105306298+samubustos@users.noreply.github.com>
2024-04-05 20:58:52 -0300

    final commit

--- prueba/src/main/java/com/pruebatecnica/prueba/entities/Role.java
+++ prueba/src/main/java/com/pruebatecnica/prueba/entities/Role.java
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 package com.pruebatecnica.prueba.entities;
 
 public enum Role {
     ADMIN,
     USER
 }
-=======
-package com.pruebatecnica.prueba.entities;
 
-public enum Role {
-    ADMIN,
-    USER
-}
->>>>>>> origin/main
