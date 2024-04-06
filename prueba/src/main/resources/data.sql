12d2905b8af340dc8078a698276cb57a3a78fcfc
Samuel Bustos <105306298+samubustos@users.noreply.github.com>
2024-04-05 21:11:18 -0300

    Update data.sql

--- prueba/src/main/resources/data.sql
+++ prueba/src/main/resources/data.sql
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-
-
 INSERT INTO Equipo (nombre, liga, pais)
 VALUES 
     ('Real Madrid', 'La Liga', 'España'),
@@ -30,36 +27,3 @@ VALUES
 
 INSERT INTO users (username, password, firstname, lastname, role)
  VALUES ('test','$2a$10$zTnGlInk6ojiit4Q7W3Tu.r8QRvMym.FRiukDtnqtOEiDqt3HHsLe','Samuel','Bustos','ADMIN')
-=======
-@CREATE ALIAS IF NOT EXISTS BCryptPasswordEncoder FOR "org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder.encode(java.lang.CharSequence)";
-
-INSERT INTO Equipo (nombre, liga, pais)
-VALUES 
-    ('Real Madrid', 'La Liga', 'España'),
-    ('FC Barcelona', 'La Liga', 'España'),
-    ('Manchester United', 'Premier League', 'Inglaterra'),
-    ('Liverpool FC', 'Premier League', 'Inglaterra'),
-    ('Juventus FC', 'Serie A', 'Italia'),
-    ('AC Milan', 'Serie A', 'Italia'),
-    ('Bayern Munich', 'Bundesliga', 'Alemania'),
-    ('Borussia Dortmund', 'Bundesliga', 'Alemania'),
-    ('Paris Saint-Germain', 'Ligue 1', 'Francia'),
-    ('Olympique de Marseille', 'Ligue 1', 'Francia'),
-    ('FC Porto', 'Primeira Liga', 'Portugal'),
-    ('Sporting CP', 'Primeira Liga', 'Portugal'),
-    ('Ajax Amsterdam', 'Eredivisie', 'Países Bajos'),
-    ('Feyenoord', 'Eredivisie', 'Países Bajos'),
-    ('Celtic FC', 'Scottish Premiership', 'Escocia'),
-    ('Rangers FC', 'Scottish Premiership', 'Escocia'),
-    ('Galatasaray SK', 'Süper Lig', 'Turquía'),
-    ('Fenerbahçe SK', 'Süper Lig', 'Turquía'),
-    ('FC Zenit Saint Petersburg', 'Rusa Premier League', 'Rusia'),
-    ('Spartak Moscow', 'Rusa Premier League', 'Rusia'),
-    ('SL Benfica', 'Primeira Liga', 'Portugal'),
-    ('Besiktas JK', 'Süper Lig', 'Turquía'),
-    ('SSC Napoli', 'Serie A', 'Italia'),
-    ('Atlético Madrid', 'La Liga', 'España');
-
-INSERT INTO users (username, password, firstname, lastname, role)
-VALUES ('test',CALL BCryptPasswordEncoder('1234'),'Samuel','Bustos')
->>>>>>> origin/main
