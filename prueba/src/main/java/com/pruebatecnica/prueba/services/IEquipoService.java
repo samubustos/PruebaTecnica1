739c93cca7828555f26f54f1224243981ea3e496
Samuel Bustos <105306298+samubustos@users.noreply.github.com>
2024-04-05 21:08:37 -0300

    final commit

--- prueba/src/main/java/com/pruebatecnica/prueba/services/IEquipoService.java
+++ prueba/src/main/java/com/pruebatecnica/prueba/services/IEquipoService.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.pruebatecnica.prueba.services;
 
 import com.pruebatecnica.prueba.entities.Equipo;
@@ -12,18 +11,3 @@ public interface IEquipoService {
     void remove(Long id);
     Equipo update(Long id, Equipo equipo);
 }
-=======
-package com.pruebatecnica.prueba.services;
-
-import com.pruebatecnica.prueba.entities.Equipo;
-import java.util.List;
-
-public interface IEquipoService {
-    List<Equipo> getAll();
-    Equipo getById(Long id);
-    List<Equipo> searchByName(String nombre);
-    void save(Equipo equipo);
-    void remove(Long id);
-    Equipo update(Long id, Equipo equipo);
-}
->>>>>>> origin/main
